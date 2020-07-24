fd8eb378386f87c4fe2ea296cca87ed6d8110d9c
Hideo Daikoku <neildaikoku@gmail.com>
2020-07-25 08:10:18 +0900

    update R code for MDS

--- mds.r
+++ mds.r
@@ -5,13 +5,13 @@ library('textshape')
 setwd('~/Documents/Work/Global Jukebox/')
 
 # load csv file
-indian_dataset <- read.csv('./data/indian_music_full.csv', header = TRUE)
+indian_dataset <- read.csv('./data/all_india_full.csv', header = TRUE)
 
 # drop unnecessary columns
 indian_subset = subset(indian_dataset, select=-c(lat, lng, culture, region))
 
 # set rownames
-column_to_rownames(indian_subset, 'canto_coding_id') 
+indian_subset<-column_to_rownames(indian_subset, 'canto_coding_id') 
 
 # distance matrix
 indian.dist <- dist(indian_subset, method="euclidean")
@@ -21,10 +21,18 @@ fit <- data.frame(cmdscale(indian.dist))
 
 culture <- indian_dataset$culture
 region <- indian_dataset$region
+canto_coding_id <- indian_dataset$canto_coding_id
 
 # plot
-ggplot(fit, aes(x=X1, y=X2)) 
-+ geom_label(aes(fill=region, label=culture)) 
-+ ggtitle("Two-dimensional Multi-Dimensional Scaling Plot of Indian Dataset") 
-+ xlab("Dimension 2") 
-+ ylab("Dimension 1")
+ggplot(fit, aes(x=X1, y=X2)) + 
+geom_label(aes(fill=region, label=culture)) + 
+ggtitle("Two-dimensional Multi-Dimensional Scaling Plot of Indian Dataset") +
+xlab("Dimension 2")+ 
+ylab("Dimension 1")
+
+# plot with canto coding id
+# ggplot(fit, aes(x=X1, y=X2))+ 
+# geom_label(aes(fill=region, label=canto_coding_id))+ 
+# ggtitle("Two-dimensional Multi-Dimensional Scaling Plot of Indian Dataset") + 
+# xlab("Dimension 2") + 
+# ylab("Dimension 1")
