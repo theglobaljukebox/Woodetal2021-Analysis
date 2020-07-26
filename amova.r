ca73d7c1fb5f68aa6f57bed94c13f60e8fce85ac
Hideo Daikoku <neildaikoku@gmail.com>
2020-07-26 12:51:13 +0900

    update global amova

--- amova.r
+++ amova.r
@@ -17,4 +17,22 @@ df = subset(df, select=-c(X))
 factors <- df[['region']]
 indianCanto$structures <- data.frame(region = as.factor(factors))
 
-amova(indianCanto$samples, indianCanto$distances, indianCanto$structures)
+indianCanto$amova <- amova(indianCanto$samples, indianCanto$distances, indianCanto$structures)
+
+
+globalCanto <- list("samples", "distances", "structures")
+df <- read.csv('./data/global_samples.csv')
+globalCanto$samples = subset(df, select = -c(X))
+globalCanto$samples <- column_to_rownames(globalCanto$samples, "canto_coding_id")
+
+df <- read.csv('./data/global_no_singles.csv')
+df <- subset(df, select = -c(X, Culture, C_cid, orv_1,orv_2,ensemble_value_id,ensemble_value_label,instrument_value_id,instrument_value_label,Number.of.Samples))
+df <- column_to_rownames(df, "canto_coding_id")
+globalCanto$distances <- dist(df)
+
+df <- read.csv('./data/global_structure.csv')
+df = subset(df, select=-c(X))
+factors <- df[['region']]
+globalCanto$structures <- data.frame(region = as.factor(factors))
+
+globalCanto$amova <- amova(globalCanto$samples, globalCanto$distances, globalCanto$structures)
