d2b83067ea0249117a8b13f3627fd4579b99be2e
Hideo Daikoku <neildaikoku@gmail.com>
2022-05-10 15:59:09 +0900

    separated conversion code

--- generate_modal_profiles.py
+++ generate_modal_profiles.py
@@ -75,26 +75,6 @@ def get_single_modal_profiles():
     print("Saving Data")
     final_df.to_csv('./output/modal_profiles.csv')
     print("Modal Profile saved in output/modal_profiles.csv")
-    print("Creating converted modal profiles")
-    convert_modal_profiles()
-    print("Modal Profile saved in output/converted_modal_profiles.csv")
-
-def get_code(line, raw_encoding):
-    for i in range(len(conversion_guide[line])):
-        if(raw_encoding==conversion_guide[line][i]['code']):
-            display_code = conversion_guide[line][i]['display_code']
-            return display_code
-
-def convert_modal_profiles():
-    df = pd.read_csv('./output/modal_profiles.csv', index_col=0)
-    f = open ('./data/conversion_guide.json', "r")
-    conversion_guide = json.loads(f.read())
-    for i in range(len(df)):
-        for j in range(1,38):
-            col_name = 'line_'+str(j)
-            code = df.loc[[i],[col_name]].values[0][0]
-            df.loc[[i],[col_name]] = get_code(col_name,code)
-    df.to_csv(',/output/converted_modal_profiles.csv',index=False)
 
 if __name__ == '__main__':
     get_single_modal_profiles()
